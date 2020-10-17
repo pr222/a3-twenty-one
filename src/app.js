b384338f5e7d036f4df9a4d41851252f6d94477e
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-10-17 15:03:15 +0200

    Move command argument validation to new file

--- src/app.js
+++ src/app.js
@@ -7,25 +7,17 @@
  * @version 1.0.0
  */
 
-// TODO: Replace the code below with your own game logic.
-
 import { Deck } from './Deck.js'
+import { inputValidation } from './inputValidation.js'
 
-let players = process.argv.slice(2)
-players = Number(players)
-// Begins game with 3 players if no starting arguments are present.
-if (players === 0) {
-  players = 3
-}
-// Checks if input arguments are valid numbers for the game to start.
-if (Number.isInteger(players) && (players <= 7 || players === 20 || players === 50)) {
-  console.log('Welcome to the table, have a seat!')
-} else {
-  console.log('Sorry, this table is not fit for you!')
+try {
+  const players = inputValidation()
+  console.log('Args to use: ', players)
+  console.log('All args ', process.argv)
+} catch (err) {
+  console.error(err.message)
 }
 
-console.log('Args to use: ', players)
-console.log('All args ', process.argv)
 /*
 try {
   // Create 52 playing cards and...
---
+++ src/inputValidation.js
@@ -0,0 +1,23 @@
+
+/**
+ * Takes the commando agruments and checks if they are valid for the game.
+ *
+ * @throws {Error} - Throws custom error if input of players are not valid.
+ * @returns {number} - Returns the number of how many players there will be in the game.
+ */
+export function inputValidation () {
+  let players = process.argv.slice(2)
+
+  players = Number(players)
+
+  // Begins game with 3 players if no starting arguments are present.
+  if (players === 0) {
+    players = 3
+  }
+  // Checks if input arguments are valid numbers for the game to start.
+  if (Number.isInteger(players) && (players <= 7 || players === 20 || players === 50)) {
+    return players
+  } else {
+    throw new Error('Sorry, this table is not fit for you!')
+  }
+}
