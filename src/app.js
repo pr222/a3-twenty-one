85cc46195737df1d5591927aa21f44ce0e60132d
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-10-23 12:24:00 +0200

    Create a discardpile and drawpile

--- src/app.js
+++ src/app.js
@@ -7,16 +7,52 @@
  * @version 1.0.0
  */
 
-// import { Deck } from './Deck.js'
+import { Deck } from './Deck.js'
 import { inputValidation } from './inputValidation.js'
 import { InputError } from './InputError.js'
 import { EmptyDeck } from './EmptyDeck.js'
 
 try {
   // Begin checking if input is a valid number of players.
-  const players = inputValidation()
-  console.log('Args to use: ', players) // TEST
-  console.log('All args ', process.argv) // TEST
+  const nrOfPlayers = inputValidation()
+  console.log('Players: ', nrOfPlayers) // TEST
+
+  // Make array of players
+  const players = []
+
+  for (let i = 0; i < nrOfPlayers; i++) {
+    players.push(i)
+  }
+
+  console.log(players) // TEST
+
+  // Create starting library with 52 playing cards.
+  const library = Deck.create()
+  console.log('Library: ', library.join(', '), '\n')
+
+  // Shuffle the library.
+  Deck.shuffle(library)
+  console.log('Shuffled library: ', library.join(', '), '\n') // TEST
+
+  // Put the library to the drawpile.
+  const drawPile = library.splice(0, 52)
+
+  console.log('Library: ', library.join(', '), '\n') // TEST
+  console.log('Drawpile: ', drawPile.join(', '), '\n') // TEST
+
+  // Create an starting empty discard pile.
+  const discardPile = []
+
+  // Draw three playing cards, view the remaining playing cards, the drawn playing cards and
+  // then calculate the value of them.
+  // (`value + playingCard` implicitly calls PlayingCard#valueOf to get
+  //  the primitive value of the current PlayingCard object.)
+  const hand = drawPile.splice(0, 3)
+
+  console.log('Deck with removed cards:', drawPile.join(', '), '\n') // TEST
+
+  const value = hand.reduce((value, playingCard) => value + playingCard, 0)
+  console.log(`Player's hand is: ${hand.join(' ')} (${value})`)
 } catch (err) {
   console.error(err.message)
   process.exitCode = 1
@@ -29,28 +65,3 @@ try {
     process.exitCode = 27
   }
 }
-
-/*
-try {
-  // Create 52 playing cards and...
-  const playingCards = Deck.create()
-  console.log(playingCards.join(', '), '\n')
-
-  // ...shuffle them.
-  Deck.shuffle(playingCards)
-  console.log(playingCards.join(', '), '\n')
-
-  // Draw three playing cards, view the remaining playing cards, the drawn playing cards and
-  // then calculate the value of them.
-  // (`value + playingCard` implicitly calls PlayingCard#valueOf to get
-  //  the primitive value of the current PlayingCard object.)
-  const hand = playingCards.splice(0, 3)
-
-  console.log(playingCards.join(', '))
-
-  const value = hand.reduce((value, playingCard) => value + playingCard, 0)
-  console.log(`${hand.join(' ')} (${value})`)
-} catch (e) {
-  console.error(e.message)
-}
-*/
